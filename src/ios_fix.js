43c1886d36c270b61aa30dfe57b62f0150c0e89b
Harry Mellsop <harrymellsop@gmail.com>
2020-07-13 21:06:32 +1200

    Mobile safari fixed?...

--- src/ios_fix.js
+++ src/ios_fix.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     var webkit = !!ua.match(/WebKit/i);
     var iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
     
-    if (true) {
+    if (iOSSafari) {
         $('#more').css({top: '90vh'});
     }
 })
