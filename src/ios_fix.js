1eb1b21ac62611ec631b964c719edcbbfb3c81b0
Harry Mellsop <harrymellsop@gmail.com>
2020-07-13 21:00:07 +1200

    Hopefully Safari now works as intended

--- src/ios_fix.js
+++ src/ios_fix.js
@@ -1,5 +1,11 @@
-// window.onresize = function() {
-//     document.body.height = window.innerHeight;
-//     document.getElementById("splash").height = window.height;
-// }
-// window.onresize();
+$(document).ready(function() {
+
+    var ua = window.navigator.userAgent;
+    var iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
+    var webkit = !!ua.match(/WebKit/i);
+    var iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
+    
+    if (iOSSafari) {
+        $('#greeting').css({bottom: '150px'});
+    }
+})
